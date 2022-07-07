aa966aa4a63386644e630ffdefa0e7649fdeb695
SIVA KUMAR <SIVA KUMAR@DESKTOP-R83QAQH>
2022-07-07 17:12:33 +0530

    Build1

---
+++ accessspecifier/src/asp/ASp2.java
@@ -0,0 +1,14 @@
+package asp;
+public class ASp2
+{public static void main(String[] args)
+{
+Asp1 f1=new Asp1();
+f1.car();
+f1.bike();
+f1.cycle();
+}
+
+
+
+}
+
---
+++ accessspecifier/src/asp/Asp1.java
@@ -0,0 +1,23 @@
+package asp;
+
+ public class Asp1
+{
+private void atm()
+{
+	System.out.println("dad kaaa atmuuu");
+}
+void car()
+{
+	System.out.println("dad kaaa caruuuu");
+	
+}
+protected void bike()
+{
+	System.out.println("dad kaaa bikuuu");
+
+}
+public void cycle()
+{System.out.println("dad kaaa cycleuuu");
+}
+}
+
